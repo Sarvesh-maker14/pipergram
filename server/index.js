c35eb7a361e864c70c20134947e937be48b8b945
Sarvesh Singh <85078565+Sarvesh-maker14@users.noreply.github.com>
2023-01-17 23:30:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(morgan("common"));
 app.use(cookieParser());
-let origin = 'http://localhost:3000';
+let origin = 'http://15.206.122.164:3000';
 console.log('here env', process.env.NODE_ENV);
 if(process.env.NODE_ENV === 'production') {
     origin = process.env.CLIENT_ORIGIN;
