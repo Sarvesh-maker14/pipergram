41c5b4816d36121bce95cfb94bcce522d2f0f3a2
Sarvesh Singh <85078565+Sarvesh-maker14@users.noreply.github.com>
2023-04-16 13:18:29 +0530

    Update dbConnect.js

--- server/dbConnect.js
+++ server/dbConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 module.exports = async () => {
   const mongoUri =
-    "mongodb+srv://sarvesh:ge9D-vTwYf9ia6H@cluster0.hqs2vev.mongodb.net/?retryWrites=true&w=majority";
+    "url";
   try {
     const connect = await mongoose.connect(mongoUri, {
       useNewUrlParser: true,
